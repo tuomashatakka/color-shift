15dc0f62216af64b92c56587a58ec21599608005
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2018-06-23 20:55:18 +0300

    Refactor project structure
    Include a .npmignore file as well as a build script for transpiling
    and writing the contents of a dist folder. Move the source js files
    into a dedicated src directory. Include package.json's recommended
    fields that were missing.

---
+++ scripts/build.js
@@ -0,0 +1,74 @@
+#!/usr/local/bin/node
+const { readdirSync:  readdir,
+        existsSync:   exists,
+        mkdirSync:    mkdir,
+        statSync:     stats,
+        writeFile }     = require('fs')
+const { resolve }       = require('path')
+const { transformFile } = require('@babel/core')
+
+
+const DEST_PATH = resolve(__dirname, '../dist')
+const SRC_PATH  = resolve(__dirname, '../src')
+const SRC_FILES = readdir(SRC_PATH)
+
+
+const directoryExists = path =>
+  exists(path) &&
+  stats(path).isDirectory()
+
+const createDirectoryIfNotExists = path => {
+  const shouldMake = !directoryExists(path)
+  if (shouldMake)
+    mkdir(path)
+  return shouldMake
+}
+
+const except = error =>
+  console.error("Error:", error)
+
+
+function transpile (filename) {
+
+  const path = resolve(SRC_PATH, filename)
+  const opts = {}
+  return new Promise((resolve, reject) => {
+
+    const callback = (error, result) => error
+      ? reject(error)
+      : resolve(result.code)
+
+    console.log("Transpiling file", path)
+    transformFile(path, opts, callback)
+  })
+}
+
+
+function write (filename, content) {
+
+  const path = resolve(DEST_PATH, filename)
+  return new Promise((resolve, reject) => {
+
+    const callback = (error, result) => error
+      ? reject(error)
+      : resolve()
+
+    console.log("Writing file", path)
+    writeFile(path, content, callback)
+  })
+}
+
+
+
+// Main
+const didCreate = createDirectoryIfNotExists(DEST_PATH)
+
+if (didCreate)
+  console.log("Created a destination directory", DEST_PATH)
+else
+  console.log("Destination directory", DEST_PATH, "already exists")
+
+for (let f of SRC_FILES)
+  transpile(f)
+    .then(write.bind(null, f))
+    .catch(except)
